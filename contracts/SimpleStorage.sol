7416b865053a36037f1b78e75d0d6035eeef283b
zunigajose <zunigajose21@gmail.com>
2021-12-20 19:01:25 -0600

    Modificacion al Smart Contract hecha el 19/12/21

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -13,7 +13,9 @@ contract SimpleStorage {
   mapping(string => mapping(string => Acta)) Actas;
 
   function addActa(string memory _ipfsHash, string memory _nivel, string memory _jrv) public {
-    Actas[_jrv][_nivel] = Acta({exists: true, ipfsHash: _ipfsHash, date: block.timestamp, sender: msg.sender});
+    if (!Actas[_jrv][_nivel].exists) {
+      Actas[_jrv][_nivel] = Acta({exists: true, ipfsHash: _ipfsHash, date: block.timestamp, sender: msg.sender});
+    } 
   }
 
   function get(string memory _jrv, string memory _nivel) public view returns (Acta memory) {
